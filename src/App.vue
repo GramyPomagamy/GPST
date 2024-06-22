869415810b4f4d4311e999fde4b95608f5e4010f
Piotr Halama <skrzynka@halamix2.pl>
2024-06-23 00:45:32 +0200

    version info

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 // import HeaderItem from './components/HeaderItem.vue'
+const appVersion = import.meta.env.PACKAGE_VERSION
 </script>
 
 <template>
   <v-app>
     <v-app-bar :elevation="2">
+      <!--<template #prepend>
+        <v-app-bar-nav-icon v-if="$vuetify.display.smAndDown" /> </template
+      >-->
+      <v-img class="me-sm-8" src="favicon.ico" max-width="50" />
       <!--<v-app-bar-nav-icon />-->
-      <v-app-bar-title>GPST – Generator Piekielnie Szybkich Thumbnailsów</v-app-bar-title>
+      <v-app-bar-title v-if="$vuetify.display.smAndDown">GPST</v-app-bar-title>
+      <v-app-bar-title v-if="$vuetify.display.mdAndUp"
+        >GPST – Generator Piekielnie Szybkich Thumbnailsów</v-app-bar-title
+      >
       <v-btn stacked prepend-icon="person" to="/">Runnerzy</v-btn>
       <v-btn stacked prepend-icon="photo_camera" to="/photo">Zdjęcia</v-btn>
       <v-btn stacked prepend-icon="play_circle" to="/youtube">Youtube</v-btn>
@@ -15,11 +23,13 @@ import { RouterView } from 'vue-router'
 
     <!--<v-navigation-drawer> </v-navigation-drawer>-->
 
-    <RouterView />
+    <v-main>
+      <RouterView />
+    </v-main>
 
     <v-footer class="d-flex flex-column">
-      By Halamix2 &#9874; 2024 - {{ new Date().getFullYear() }}. Halamix nie potrafi we frontend, a
-      jednak to zrobił; what's your excuse?
+      By Halamix2 &#9874; 2024 - {{ new Date().getFullYear() }}, wersja {{ appVersion }}. Halamix
+      nie potrafi we frontend, a jednak to zrobił; what's your excuse?
     </v-footer>
   </v-app>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: { labs: true } })],
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
