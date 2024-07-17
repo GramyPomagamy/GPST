dd7b7fde6567439684bc76dcdd712115a9c93414
Piotr Halama <skrzynka@halamix2.pl>
2024-07-17 23:08:49 +0200

    paths

--- src/utils/loadImage.ts
+++ src/utils/loadImage.ts
@@ -2,7 +2,7 @@ export const loadImage = function (url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const image = new Image()
     image.onerror = reject
-    image.src = new URL(url, import.meta.url).href
+    image.src = new URL(url, import.meta.url + import.meta.env.BASE_URL).href
     resolve(image)
   })
 }
