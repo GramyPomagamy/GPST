92bd2162c071922074ad413ec5eaccb0a7639c87
Piotr Halama <skrzynka@halamix2.pl>
2024-07-18 21:29:50 +0200

    Debug on production

--- src/utils/loadImage.ts
+++ src/utils/loadImage.ts
@@ -2,6 +2,7 @@ export const loadImage = function (url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const image = new Image()
     image.onerror = reject
+    console.log('based', window.location.origin, import.meta.env.BASE_URL, url)
     image.src = new URL(url, window.location.origin + import.meta.env.BASE_URL).href
     resolve(image)
   })
