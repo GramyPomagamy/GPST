407f2ac46c361f0f5a3e302ce1b5c4a22c305d45
Piotr Halama <skrzynka@halamix2.pl>
2024-07-18 21:32:28 +0200

    Fix image paths

--- src/utils/loadImage.ts
+++ src/utils/loadImage.ts
@@ -2,7 +2,6 @@ export const loadImage = function (url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const image = new Image()
     image.onerror = reject
-    console.log('based', window.location.origin, import.meta.env.BASE_URL, url)
     image.src = new URL(url, window.location.origin + import.meta.env.BASE_URL).href
     resolve(image)
   })
