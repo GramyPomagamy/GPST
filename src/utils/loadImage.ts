879975fb5ebafd3d1d22daf0f4d1f82995a575ca
Piotr Halama <skrzynka@halamix2.pl>
2024-07-18 21:36:05 +0200

    try to fix URL generation once again
    i don't know why it's broken on GH pages

--- src/utils/loadImage.ts
+++ src/utils/loadImage.ts
@@ -2,7 +2,7 @@ export const loadImage = function (url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const image = new Image()
     image.onerror = reject
-    image.src = new URL(url, window.location.origin + import.meta.env.BASE_URL).href
+    image.src = new URL(url, window.location.origin + import.meta.env.BASE_URL + '/').href
     resolve(image)
   })
 }
