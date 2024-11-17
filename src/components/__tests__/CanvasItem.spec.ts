b90d5000bea3f3a77c7f4f8dc9ebda87ad64570b
Piotr Halama <skrzynka@halamix2.pl>
2024-11-17 22:11:32 +0100

    fix the dummy test case

--- src/components/__tests__/CanvasItem.spec.ts
+++ src/components/__tests__/CanvasItem.spec.ts
@@ -4,6 +4,8 @@ import { mount } from '@vue/test-utils'
 
 import { defaultVuetify } from '@/vuetifyInstance'
 
+import { createTestingPinia } from '@pinia/testing'
+
 import CanvasItem from '@/components/CanvasItem.vue'
 
 const vuetify = defaultVuetify()
@@ -11,14 +13,14 @@ const vuetify = defaultVuetify()
 describe('CanvasItem', () => {
   it('renders props.msg when passed', () => {
     const width = 800,
-     height = 600,
-     title = 'title',
-     wrapper = mount(CanvasItem, {
-      props: { canvasWidth: width, canvasHeight: height, title },
-      global: {
-        plugins: [vuetify]
-      }
-    })
+      height = 600,
+      title = 'title',
+      wrapper = mount(CanvasItem, {
+        props: { canvasWidth: width, canvasHeight: height, title },
+        global: {
+          plugins: [vuetify, createTestingPinia()]
+        }
+      })
     //Expect(wrapper.text()).
   })
 })
