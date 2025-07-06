dc95a1e5c93c57b0870d299dae6981e8059e81a9
Piotr Halama <skrzynka@halamix2.pl>
2025-07-06 18:37:16 +0200

    fix font laoding by including fonts

--- src/fontLoader.ts
+++ src/fontLoader.ts
@@ -1,49 +1,51 @@
 const fontURLs = {
-  BarlowCondensed:
-    'url(https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;500;600&display=swap)',
-  SairaCondensed:
-    'url(https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@600;700;800&display=swap)',
-  PTSansNarrow: 'url(https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@700&display=swap)'
+  BarlowCondensed300: 'url(fonts/barlow-condensed-v12-latin_latin-ext-300.woff2)',
+  BarlowCondensed500: 'url(fonts/barlow-condensed-v12-latin_latin-ext-500.woff2)',
+  BarlowCondensed600: 'url(fonts/barlow-condensed-v12-latin_latin-ext-600.woff2)',
+  SairaCondensed600: 'url(fonts/saira-condensed-v11-latin_latin-ext-600.woff2)',
+  SairaCondensed700: 'url(fonts/saira-condensed-v11-latin_latin-ext-700.woff2)',
+  SairaCondensed800: 'url(fonts/saira-condensed-v11-latin_latin-ext-800.woff2)',
+  PTSansNarrow700: 'url(fonts/pt-sans-narrow-v18-latin_latin-ext-700.woff2)'
 }
 
-const fontBarlowCondensedLight = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed, {
+const fontBarlowCondensedLight = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed300, {
   style: 'normal',
   weight: '300'
 })
 
-const fontBarlowCondensedMedium = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed, {
+const fontBarlowCondensedMedium = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed500, {
   style: 'normal',
   weight: '500'
 })
 
-const fontBarlowCondensedSemibold = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed, {
+const fontBarlowCondensedSemibold = new FontFace('Barlow Condensed', fontURLs.BarlowCondensed600, {
   style: 'normal',
   weight: '600'
 })
 
-const fontSairaCondensedSemibold = new FontFace('Saira Condensed', fontURLs.SairaCondensed, {
+const fontSairaCondensedSemibold = new FontFace('Saira Condensed', fontURLs.SairaCondensed600, {
   style: 'normal',
   weight: '600'
 })
 
-const fontSairaCondensedBold = new FontFace('Saira Condensed', fontURLs.SairaCondensed, {
+const fontSairaCondensedBold = new FontFace('Saira Condensed', fontURLs.SairaCondensed700, {
   style: 'normal',
   weight: '700'
 })
 
-const fontSairaCondensedExtrabold = new FontFace('Saira Condensed', fontURLs.SairaCondensed, {
+const fontSairaCondensedExtrabold = new FontFace('Saira Condensed', fontURLs.SairaCondensed800, {
   style: 'normal',
   weight: '800'
 })
 
-const fontPTSansNarrowBold = new FontFace('Saira Condensed', fontURLs.SairaCondensed, {
+const fontPTSansNarrowBold = new FontFace('Saira Condensed', fontURLs.PTSansNarrow700, {
   style: 'normal',
   weight: '700'
 })
 
 export const fonts = {
-  install() {
-    Promise.all([
+  async install() {
+    await Promise.all([
       fontBarlowCondensedLight.load(),
       fontBarlowCondensedMedium.load(),
       fontBarlowCondensedSemibold.load(),
@@ -51,14 +53,14 @@ export const fonts = {
       fontSairaCondensedBold.load(),
       fontSairaCondensedExtrabold.load(),
       fontPTSansNarrowBold.load()
-    ]).then(() => {
-      document.fonts.add(fontBarlowCondensedLight)
-      document.fonts.add(fontBarlowCondensedMedium)
-      document.fonts.add(fontBarlowCondensedSemibold)
-      document.fonts.add(fontSairaCondensedSemibold)
-      document.fonts.add(fontSairaCondensedBold)
-      document.fonts.add(fontSairaCondensedExtrabold)
-      document.fonts.add(fontPTSansNarrowBold)
-    })
+    ])
+
+    document.fonts.add(fontBarlowCondensedLight)
+    document.fonts.add(fontBarlowCondensedMedium)
+    document.fonts.add(fontBarlowCondensedSemibold)
+    document.fonts.add(fontSairaCondensedSemibold)
+    document.fonts.add(fontSairaCondensedBold)
+    document.fonts.add(fontSairaCondensedExtrabold)
+    document.fonts.add(fontPTSansNarrowBold)
   }
 }
